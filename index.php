9b1e3f16f72fb31793189eed6a3766b8a8e59b8b
SUNBURTNENUX0 <anonymus11.17280@gmail.com>
2023-08-02 17:21:46 -0600

    cominezo

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ALLEGRETTO</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+    
+    <header>
+        <div class="back">
+
+            <div class="menu container">
+
+                <a href="#" class="enlace">
+                    <img src="imagenes/logo1.png" alt="" class="logo">
+                </a>
+                <input type="checkbox" id="menu"/>
+                <label for="menu">
+                    <img src="imagenes/menu.png" class="menu-icono" alt="">
+                </label>
+
+                <nav class="navbar">
+                    <ul>
+                        <li><a href="#">Inicio</a></li>
+                        <li><a href="#">Sabores</a></li>
+                        <li><a href="#">Contactos</a></li>
+                        <li><a href="#">Nosotros</a></li>
+                    </ul>
+                </nav>
+
+            </div>
+
+        </div>
+
+    </header>
+
+</body>
+</html>
